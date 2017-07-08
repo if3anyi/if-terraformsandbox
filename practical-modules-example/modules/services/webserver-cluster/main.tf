4d6439654603e2f3ec1b32d1c8cd1eb46af58e67
ifeanyi <dennis@ifeanyi.io>
2017-07-08 15:25:12 +0100

    samples on creating modules for different environments.

--- practical-modules-example/modules/services/webserver-cluster/main.tf
+++ practical-modules-example/modules/services/webserver-cluster/main.tf
@@ -1,8 +1,10 @@
+## LAUNCH CONFIGURATION  ####
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-d7b9a2b1"
   instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.sg.id}"]
 
+  #user_data       = "${data.template_file.user_data.rendered}"
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello Again, File layout example" > index.html
@@ -14,6 +16,17 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user-data.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    #db_address  = "${data.terraform_remote_state.db.address}"
+    #db_port     = "${data.terraform_remote_state.db.port}"
+  }
+}
+
+## AUTOSCALING GROUP  ####
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones   = ["${data.aws_availability_zones.all.names}"]
@@ -31,23 +44,28 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+### SECURITY GROUP  ####
 resource "aws_security_group" "sg" {
   name = "${var.cluster_name}-instance"
-
-  ingress {
-    from_port   = "${var.server_port}"
-    to_port     = "${var.server_port}"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
+resource "aws_security_group_rule" "allow_inbound_http" {
+  type = "ingress"
+  security_group_id =  "${aws_security_group.sg.id}"
+
+  from_port   = "${var.server_port}"
+  to_port     = "${var.server_port}"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 data "aws_availability_zones" "all" {}
 
+## ELASTICLOAD BALANCER  ####
 resource "aws_elb" "example" {
   name               = "${var.cluster_name}-example"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
@@ -69,25 +87,39 @@ resource "aws_elb" "example" {
   }
 }
 
+#######################
+## SECURITY GROUP  ####
 resource "aws_security_group" "elb-sg" {
   name = "${var.cluster_name}-elb"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-#s3 bucket
+#basically allows you to create flexible modules
+# exposing this means to can now create custom rules outside
+  # the module.
+  # chapter 4 page 97/98 of Terraform Up and Running
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id =  "${aws_security_group.elb-sg.id}"
+
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id =  "${aws_security_group.elb-sg.id}"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+
+## S3 BUCKET GROUP  ####
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}"
 }
--- practical-modules-example/stage/output.tf
+++ practical-modules-example/stage/output.tf
@@ -1,7 +1,3 @@
-# output "asg_name" {
-#   value = "${aws_autoscaling_group.example.name}"
-# }
-
 output "elb_dns_name" {
   value = "${module.webserver_cluster.elb_dns_name}"
 }
